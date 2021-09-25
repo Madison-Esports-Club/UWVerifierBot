d1aa33d8157278ac2be56eb5d313a3b69f4d17d0
DMLooter <verbanmike@gmail.com>
2021-09-24 23:12:29 -0500

    Initial backend commit
    Made some things work. inserting datetimes to the db isnt quite working.

--- Cogs/verification.py
+++ Cogs/verification.py
@@ -1,27 +1,141 @@
 #Cog for verifying
 import discord
 from discord.ext import commands
+import psycopg2
+import datetime
+import requests
 
 from Cogs.db import dbconnect
 
+#verified_table_name = "verified_users"
+#verification_attempt_table_name = "verification_requests"
+
 class Verification(commands.Cog):
-    def __init__(self,bot):
-        self.bot=bot
+	def __init__(self,bot):
+		self.bot=bot
 ###########################################################################
 #Commands in cogs have different syntax than commands that arent in a cog. Example: (pasted from my own code just delete when you're done with this)
 
-    @commands.command(name="unlock") #The name is what the command used by the user is. You can also do name="unlock", aliases=["un-lock", "unlock1"]) etc to have multiple ways to use the command (always have to be in dict form)
-    @commands.has_permissions(manage_channels=True) #Optional check for permissions
-    async def unlock(self, ctx): #self and ctx always required for cogs (main difference)
-        perms=ctx.channel.overwrites_for(ctx.guild.default_role)
-        perms.send_messages=True
-        await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=perms)
-        await ctx.send(embed=discord.Embed(title=f"**{ctx.channel}** unlocked to non-admin users"))
-    
-    @unlock.error #Error handling
-    async def clear_error(self, ctx, error):
-        if isinstance(error,commands.MissingPermissions): 
-            await ctx.send(embed=discord.Embed(title="You don't have perms to do that dummy (Requires `Manage Channels` permission)"))
+	@commands.command(name="unlock") #The name is what the command used by the user is. You can also do name="unlock", aliases=["un-lock", "unlock1"]) etc to have multiple ways to use the command (always have to be in dict form)
+	@commands.has_permissions(manage_channels=True) #Optional check for permissions
+	async def unlock(self, ctx): #self and ctx always required for cogs (main difference)
+		perms=ctx.channel.overwrites_for(ctx.guild.default_role)
+		perms.send_messages=True
+		await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=perms)
+		await ctx.send(embed=discord.Embed(title=f"**{ctx.channel}** unlocked to non-admin users"))
+
+	@commands.command(name='verify')
+	async def verfiy(self, ctx, email):
+		if(discord.utils.get(ctx.author.roles, name="Verified") != None):
+			await ctx.send("you are already verified!")
+			return
+		#print(ctx.author.id)
+		verified, message = self.verify_user(ctx.author.id, email)
+		response = "Thank you for submitting your verification request, it will be processed within 24 hours."
+		await ctx.send(message)
+
+	@unlock.error #Error handling
+	async def clear_error(self, ctx, error):
+		if isinstance(error,commands.MissingPermissions):
+			await ctx.send(embed=discord.Embed(title="You don't have perms to do that dummy (Requires `Manage Channels` permission)"))
+###########################################################################
+	def verify_user(self, user_id, email):
+		"""
+		Attempts to verify the specified user with the specified email address
+
+		Args:
+			user_id: The discord userid of the user to be verified
+			email: The email address they want to use
+		Returns:
+			success, message: a boolean saying if the user was verified, and a message to return to the user.
+							Note that users who are already verified will cause this method to return false.
+		"""
+		print("verifying " +str(user_id) + " with email "+email)
+		if(self.is_verified(user_id)):
+			return False, "You're already verified!"
+
+		#Check that its a wisc.edu ending at least
+		if(email.split('@')[-1] != "wisc.edu"):
+			return False, email + " is not a wisc.edu email address."
+
+		cursor, conn = dbconnect()
+		cursor.execute("SELECT user_id FROM verified_users WHERE email = %s;", (email,))
+		if(cursor.rowcount > 0):
+			print(user_id + "Tried to verify with email " + email + " which is already in use by " + cursor.fetchone()[0])
+			return false, "That email is already in use, talk to a board member if you believe this is an error"
+
+		real, message = self.verify_email(email)
+		if(real):
+			cursor.execute("INSERT INTO verified_users (user_id, email, time) VALUES (%s, %s, %s);", (user_id, email, datetime.datetime.utcnow()))
+			return True, "Congratulations, you are now verified with the email " + email + "!"
+		if(message == "limit"):
+			return False, "Verification daily limit reached, please try again in 24 hours."
+		if(message == "Unknown"):
+			return False, "Unknown error occurred, please wait a while and try again. Reach out to a board member if this issue persists."
+
+		return False, "Sorry, the email address " + email + " is not a valid wisc.edu email address"
+###########################################################################
+	def is_verified(self, user_id):
+		"""
+		Returns whether or not the specified user is already verified
+		"""
+		print("deciding if " +str(user_id) + " is verified")
+		return self.get_verified_email(user_id) != None
+###########################################################################
+	def get_verified_email(self, user_id):
+		"""
+		Returns the verified email associated with the specified user, or None if the user is not verified
+		"""
+		print("getting " +str(user_id) + "'s verified email")
+		cursor, conn = dbconnect()
+		cursor.execute("SELECT email FROM verified_users WHERE user_id = %s;", (user_id,))
+		if(cursor.rowcount != 1):
+			return None
+		email = cursor.fetchone()[0]
+		print("got " + email)
+		conn.close()
+		return email
+###########################################################################
+	def verify_email(self, email):
+		"""
+		Attempts to verify an email address.
+		"""
+		cursor, conn = dbconnect()
+		#Check if this email has been tried recently(one week)
+		cursor.execute("SELECT * FROM verification_requests WHERE email = %s AND time > now() - interval '1 week';", (email,))
+		if(cursor.rowcount > 0):
+			return cursor.fetchone()[3], "cached"
+
+		cursor.execute("SELECT time, daily_request_number FROM verification_requests ORDER BY time DESC LIMIT 1;")
+		row = cursor.fetchone()
+		number = row[1] + 1
+		then = row[0]
+		current = datetime.datetime.utcnow()
+
+		if(then.day != current.day or then.month != current.month or then.year != current.year):
+			number = 1
+
+		if(number > 99):
+			print("HIT DAILY REQUEST LIMIT")
+			return False, "limit"
+
+		response = requests.get("https://isitarealemail.com/api/email/validate",
+	    params = {'email': email})
+
+		status = response.json()['status']
+		real = False
+		if status == "valid":
+			real = True
+		elif status == "invalid":
+			real = False
+		else:
+			print("email was unknown: " + email + ", with response: " + response)
+			return False, "Unknown"
+
+		cursor.execute("INSERT INTO verification_requests(email, time, daily_request_number, result) VALUES (%s %s %s %s);", (email, current, number, real))
+		cursor.commit()
+		conn.close()
+		return real, "from server"
 ###########################################################################
 def setup(bot):
-    bot.add_cog(Verification(bot))
+	bot.add_cog(Verification(bot))
