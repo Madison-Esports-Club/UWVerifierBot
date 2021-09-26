ab186feb28b05fed35116a66caa1d06f03c0edc6
Anton Gurevich <71106751+antgurevich@users.noreply.github.com>
2021-09-25 23:23:50 -0500

    Added bot event to give Verified role to user if they were already verified

--- UWVerification.py
+++ UWVerification.py
@@ -1,55 +1,68 @@
 import discord
+from discord import guild
 from discord.ext import commands
 import os
 import random
 import json
 from configparser import ConfigParser
 
-bot=commands.Bot(command_prefix=commands.when_mentioned_or("!")
-				,case_insensative=True
-#                ,owner_id=
-				,intents=discord.Intents().all())
-bot.remove_command("help") #To have your own help command you have to remove the built-in discord one
-###########################################################################     #Personally have become used to using 75 #s to seperate functions for readability
+from Cogs.db import dbconnect
+
+bot=commands.Bot(command_prefix = commands.when_mentioned_or("!")
+				,case_insensative = True
+                		,owner_id = (139813446618185728, 338707493188272129) #DMLooter#4251, PureCache#0001
+				,intents = discord.Intents().all())
+bot.remove_command("help") #Removes discord built-in help command
+###########################################################################
 @bot.event #Loads all cogs and initiates bot
 async def on_ready():
-
 	with open("UWVerificationHelp.json","r") as cogFile:
-		data=json.load(cogFile)
-	data=data["Cogs"]
-	cogList=list(data.keys())
+		data = json.load(cogFile)
+	data = data["Cogs"]
+	cogList = list(data.keys())
 
 	for cog in cogList:
-		cog=("Cogs."+cog)
+		cog = ("Cogs." + cog)
 		try: #Loads each cog
 			bot.load_extension(cog)
-			print ("Loaded",cog)
+			print ("Loaded", cog)
 		except Exception as e: #If cog can't be loaded, will error to console
-			print ("Error loading",cog,"e:",e)
+			print ("Error loading", cog, "e:", e)
 
-	randNum=random.randint(1,2) #Bot status
+	randNum= random.randint(1, 2) #Bot status
 	if randNum==1:
-		await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Madison Gaming & Esports"))
+		await bot.change_presence(activity = discord.Activity(type = discord.ActivityType.watching, name = "Madison Gaming & Esports"))
 	else:
-		await bot.change_presence(activity=discord.Game("video games with Badgers"))
+		await bot.change_presence(activity = discord.Game("video games with Badgers"))
 
-	print (bot.user.name,"successfully connected to Discord")
+	print (bot.user.name, "successfully connected to Discord")
 ###########################################################################
-@bot.event #If user just types '!verify'
+@bot.event #Gives Verified role to user if they are in the db already 
+async def on_member_join(member):
+    cursor, conn = dbconnect()
+    cursor.execute("SELECT user_id FROM verified_users WHERE user_id = %s;", (member.id,))
+    result = cursor.fetchone()
+    conn.close()
+
+    if result is not None:
+        role = discord.utils.get(member.guild.roles, id = 887379146857144461) #Currently a test role, replace id with correct Verified role when added to main server
+        await member.add_roles(role) 
+###########################################################################
+@bot.event #If user just types '!'
 async def on_message(message):
-	if message.author==bot.user: #So bot doesn't respond to itself
+	if message.author == bot.user: #Ensures bot doesn't respond to itself
 		return
 
-	if message.content=="!":
-		await message.channel.send(embed=discord.Embed(title=f"Hello {message.author}! Use `!v help` to learn more about me!"))
+	if message.content == "!":
+		await message.channel.send(embed = discord.Embed(title = f"Hello {message.author}! Use `!v help` to learn more about me!"))
 	print(message.content)
 	await bot.process_commands(message) #Enables commands
 ###########################################################################
 try: #Config var in Heroku
 	bot.run(os.environ["DISCORD_TOKEN"])
 except: #Runs from system
-	config_object=ConfigParser()
+	config_object = ConfigParser()
 	config_object.read("BotVariables.ini")
-	variables=config_object["variables"]
-	DISCORD_TOKEN=variables["DISCORD_TOKEN"]
+	variables = config_object["variables"]
+	DISCORD_TOKEN = variables["DISCORD_TOKEN"]
 	bot.run(DISCORD_TOKEN)
