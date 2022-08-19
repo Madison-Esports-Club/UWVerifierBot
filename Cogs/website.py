af0e76d220004f69b004d6f0f2a70b4948bf1951
DMLooter <verbanmike@gmail.com>
2022-08-19 18:29:46 -0500

    Added Inhouse Command

--- Cogs/website.py
+++ Cogs/website.py
@@ -5,15 +5,15 @@ from configparser import ConfigParser
 import os
 from datetime import datetime
 import httpx
-from discord.ext import commands
+from discord.ext import commands, bridge
 
 class Website(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-###########################################################################
+    ###########################################################################
     @discord.slash_command(description = "Creates a new event on the website calendar.", guild_ids = [887366492730036276])
     @commands.has_any_role('Board Member', 'Game Officer', 'Bot Technician', 'Mod', 'Faculty Advisor')
-    async def create_event(self, ctx, name:discord.Option(str), location:discord.Option(str), game:discord.Option(str), date:discord.Option(str), time:discord.Option(str), ampm:discord.Option(str)):
+    async def createevent(self, ctx, name:discord.Option(str), location:discord.Option(str), game:discord.Option(str), date:discord.Option(str), time:discord.Option(str), ampm:discord.Option(str)):
         await ctx.defer()
         logEmbed = discord.Embed(title = "New Event", color = discord.Color.teal())
 
@@ -34,17 +34,22 @@ class Website(commands.Cog):
             "Calendar": calendar,
             "Start": start.isoformat()
         }
-        await sendPost("NewEvent", data) # TODO Add failure mode
+        status = await sendPost("NewEvent", data)
 
-        logEmbed.add_field(name=("*Name*"),value = name, inline=False)
-        logEmbed.add_field(name=("*Location*"),value = location, inline=False)
-        logEmbed.add_field(name=("*Calendar*"),value = calendar, inline=False)
-        logEmbed.add_field(name=("*Starts*"),value = start.isoformat(), inline=False)
+        if(status == 200):
+            print(f"{ctx.user.name} Created an event {name} on calendar {calendar}")
+            logEmbed.add_field(name=("*Name*"),value = name, inline=False)
+            logEmbed.add_field(name=("*Location*"),value = location, inline=False)
+            logEmbed.add_field(name=("*Calendar*"),value = calendar, inline=False)
+            logEmbed.add_field(name=("*Starts*"),value = start.isoformat(), inline=False)
 
-        await ctx.respond(embed = logEmbed)
+            await ctx.respond(embed = logEmbed)
+        else:
+            await ctx.respond(content = "Failed to create event")
 
-    @create_event.error
-    async def create_event_error(self, ctx, error):
+
+    @createevent.error
+    async def createevent_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.respond(embed = discord.Embed(title = "Missing required argument", description = "Correct usage: !createevent \"<name>\" \"<location>\" \"<description>\" mm/dd/yy HH:MM AM/PM", color = discord.Color.red()))
         elif isinstance(error, commands.MissingAnyRole):
@@ -54,6 +59,179 @@ class Website(commands.Cog):
             await ctx.respond(embed = discord.Embed(title = "Unknown error. Please contact developers to check logs", color = discord.Color.red()))
             print("Create Event error: ", error)
             raise error
+    ###########################################################################
+    @discord.slash_command(description = "Changes the Inhouse Schedule for a game", guild_ids = [887366492730036276])
+    @commands.has_any_role('Board Member', 'Game Officer', 'Bot Technician', 'Mod', 'Faculty Advisor')
+    async def changeinhouse(self, ctx):
+        await ctx.respond("Select the new Inhouse time and date", view=InhouseView())
+###########################################################################
+class InhouseView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.game = ""
+        self.day = -1
+        self.time = ""
+
+    @discord.ui.select( # the decorator that lets you specify the properties of the select menu
+        row = 0,
+        placeholder = "Choose a Game", # the placeholder text that will be displayed if nothing is selected
+        min_values = 1, # the minimum number of values that must be selected by the users
+        max_values = 1, # the maxmimum number of values that can be selected by the users
+        options = [ # the list of options from which users can choose, a required field
+            discord.SelectOption(
+                label="League of Legends"
+            ),
+            discord.SelectOption(
+                label="Valorant"
+            ),
+            discord.SelectOption(
+                label="Rainbow 6"
+            ),
+            discord.SelectOption(
+                label="Overwatch"
+            ),
+            discord.SelectOption(
+                label="CS:GO"
+            ),
+            discord.SelectOption(
+                label="Smite"
+            ),
+            discord.SelectOption(
+                label="Rocket League"
+            ),
+            discord.SelectOption(
+                label="DotA 2"
+            ),
+            discord.SelectOption(
+                label="Call of Duty"
+            ),
+            discord.SelectOption(
+                label="Apex Legends"
+            )
+        ]
+    )
+    async def game_select_callback(self, select, interaction): # the function called when the user is done selecting options
+        self.game = select.values[0]
+        await interaction.response.defer()
+
+    @discord.ui.select( # the decorator that lets you specify the properties of the select menu
+        row = 1,
+        placeholder = "Choose a Day of the Week", # the placeholder text that will be displayed if nothing is selected
+        min_values = 1, # the minimum number of values that must be selected by the users
+        max_values = 1, # the maxmimum number of values that can be selected by the users
+        options = [ # the list of options from which users can choose, a required field
+            discord.SelectOption(
+                label="Sunday"
+            ),
+            discord.SelectOption(
+                label="Monday"
+            ),
+            discord.SelectOption(
+                label="Tuesday"
+            ),
+            discord.SelectOption(
+                label="Wednesday"
+            ),
+            discord.SelectOption(
+                label="Thursday"
+            ),
+            discord.SelectOption(
+                label="Friday"
+            ),
+            discord.SelectOption(
+                label="Saturday"
+            )
+        ]
+    )
+    async def day_select_callback(self, select, interaction): # the function called when the user is done selecting options
+        self.day = DayOfWeek.index(select.values[0])
+        await interaction.response.defer()
+
+    @discord.ui.select( # the decorator that lets you specify the properties of the select menu
+        row = 2,
+        placeholder = "Choose a Time of Day", # the placeholder text that will be displayed if nothing is selected
+        min_values = 1, # the minimum number of values that must be selected by the users
+        max_values = 1, # the maxmimum number of values that can be selected by the users
+        options = [ # the list of options from which users can choose, a required field
+            discord.SelectOption(
+                label="8:00am"
+            ),
+            discord.SelectOption(
+                label="9:00am"
+            ),
+            discord.SelectOption(
+                label="10:00am"
+            ),
+            discord.SelectOption(
+                label="11:00am"
+            ),
+            discord.SelectOption(
+                label="12:00pm"
+            ),
+            discord.SelectOption(
+                label="1:00pm"
+            ),
+            discord.SelectOption(
+                label="2:00pm"
+            ),
+            discord.SelectOption(
+                label="3:00pm"
+            ),
+            discord.SelectOption(
+                label="4:00pm"
+            ),
+            discord.SelectOption(
+                label="5:00pm"
+            ),
+            discord.SelectOption(
+                label="6:00pm"
+            ),
+            discord.SelectOption(
+                label="7:00pm"
+            ),
+            discord.SelectOption(
+                label="8:00pm"
+            ),
+            discord.SelectOption(
+                label="9:00pm"
+            ),
+            discord.SelectOption(
+                label="10:00pm"
+            )
+        ]
+    )
+    async def time_select_callback(self, select, interaction): # the function called when the user is done selecting options
+        self.time = select.values[0]
+        await interaction.response.defer()
+
+    @discord.ui.button(label="Confirm", style=discord.ButtonStyle.success, emoji="✅") # Create a button with the label "😎 Click me!" with color Blurple
+    async def confirm_callback(self, button, interaction):
+        complete = True
+        for child in self.children: # loop through all the children of the view
+            if(hasattr(child, "values") and len(child.values) == 0):
+                complete = False
+
+        if complete:
+            data = {
+                "Game": self.game,
+                "FirstDay": datetime.utcnow().isoformat(),
+                "StartTime": datetime.strptime(self.time, "%I:%M%p").isoformat(),
+                "DayOfWeek": self.day
+            }
+
+            status = await sendPost("NewInhouse", data)
+            if(status == 200):
+                print(f"{interaction.user.name} changed {self.game} Inhouses to {DayOfWeek[self.day]}s at {self.time}")
+                await interaction.message.edit(content = f"Changed {self.game} Inhouses to {DayOfWeek[self.day]}s at {self.time}", view = None)
+            else:
+                await interaction.message.edit(content = f"Failed to change Inhouse time, please try again or contact the Devs", view = None)
+        else:
+            await interaction.message.edit(content = "Select the new Inhouse time and date. (Fill in all fields!)")
+
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.danger, emoji="❌") # Create a button with the label "😎 Click me!" with color Blurple
+    async def cancel_callback(self, button, interaction):
+        await interaction.message.delete()
+
 ###########################################################################
 async def sendPost(endpoint, json):
     try: #Config var in Heroku
@@ -71,8 +249,11 @@ async def sendPost(endpoint, json):
         try:
             print(resp.json())
         except ValueError:
-            return
+            return resp.status_code
+        return resp.status_code
 ###########################################################################
+DayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
 LoLNames = ["lol", "league", "league of legends"]
 ValorantNames = ["valorant","val"]
 R6Names = ["Rainbow six", "rainbow 6", "rainbow six siege", "rainbow 6 siege", "r6", "r6 siege"]
